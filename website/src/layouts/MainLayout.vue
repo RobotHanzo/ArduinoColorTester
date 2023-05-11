16837cc3d52099686f4c7f6d235d837e89ca346d
RobotHanzo <RobotHanzo@users.noreply.github.com>
2023-05-12 00:34:17 +0800

    Reformat

--- website/src/layouts/MainLayout.vue
+++ website/src/layouts/MainLayout.vue
@@ -1,53 +1,139 @@
 <template>
-    <q-layout view="lHh Lpr lFf">
-        <q-header elevated>
-            <q-toolbar>
-                <q-btn
-                        flat
-                        dense
-                        round
-                        icon="menu"
-                        aria-label="Menu"
-                        @click="toggleLeftDrawer"
-                />
+  <q-layout view="hHh LpR fFf">
+    <q-header class="bg-primary text-white" elevated>
+      <q-toolbar>
+        <q-btn dense flat icon="menu" round @click="toggleLeftDrawer"/>
+        <q-toolbar-title>
+          <q-avatar>
+            <img alt="Icon" src="favicon.ico"/>
+          </q-avatar>
+          Jerry Control Panel
+        </q-toolbar-title>
+        <q-btn
+          dense
+          flat
+          icon="light_mode"
+          round
+          style="align-content: end"
+          @click="
+            $q.dark.toggle();
+            $q.localStorage.set('dark', $q.dark.isActive);
+          "
+        />
+      </q-toolbar>
+    </q-header>
 
-                <q-toolbar-title>
-                    Quasar App
-                </q-toolbar-title>
-
-                <div>Quasar v{{ $q.version }}</div>
-            </q-toolbar>
-        </q-header>
-
-        <q-drawer
-                v-model="leftDrawerOpen"
-                show-if-above
-                bordered
+    <q-drawer v-model="leftDrawerOpen" elevated show-if-above side="left">
+      <q-list class="menu-list fit column">
+        <q-item
+          v-for="endpoint of endpoints.filter((e) => {
+            return e.permission === undefined || hasPermission(e.permission);
+          })"
+          :key="endpoint.name"
+          v-ripple
+          :active="selected === endpoint.name"
+          :to="endpoint.url"
+          @click="changeSelected(endpoint.name)"
         >
-            <q-list>
-                <q-item-label
-                        header
-                >
-                    Essential Links
-                </q-item-label>
+          <q-item-section avatar>
+            <q-icon :name="endpoint.icon"/>
+          </q-item-section>
 
+          <q-item-section>
+            <q-item-label>{{ endpoint.name }}</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-space/>
 
-            </q-list>
-        </q-drawer>
+        <q-item v-ripple>
+          <q-menu>
+            <div class="row no-wrap q-pa-md">
+              <div class="column">
+                <div class="text-h6 q-mb-md">Settings</div>
+                <q-toggle v-model="mobileData" label="Use Mobile Data"/>
+                <q-toggle v-model="bluetooth" label="Bluetooth"/>
+              </div>
 
-        <q-page-container>
-            <router-view/>
-        </q-page-container>
-    </q-layout>
+              <q-separator class="q-mx-lg" inset vertical/>
+
+              <div class="column items-center">
+                <q-avatar size="72px">
+                  <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+                </q-avatar>
+
+                <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+                <q-btn
+                  v-close-popup
+                  color="primary"
+                  label="Logout"
+                  push
+                  size="sm"
+                />
+              </div>
+            </div>
+          </q-menu>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <q-page class="row no-wrap">
+        <div class="col q-ma-md">
+          <div class="column full-height">
+            <q-scroll-area class="col q-pa-sm" visible>
+              <router-view/>
+            </q-scroll-area>
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import {ref} from 'vue';
+import {useRouter} from 'vue-router';
 
-
-const leftDrawerOpen = ref(false)
+const router = useRouter();
+let leftDrawerOpen = ref(false);
+let endpoints = [
+  {
+    name: 'Aliases',
+    url: '/aliases',
+    icon: 'bookmark',
+    permission: 'aliases.add',
+  },
+  {
+    name: 'Prices',
+    url: '/prices',
+    icon: 'attach_money',
+    permission: 'prices.set',
+  },
+  {
+    name: 'Scammers',
+    url: '/scammers',
+    icon: 'accessible_forward',
+    permission: 'scammers.add',
+  },
+];
+let selected = ref('');
+for (let endpoint of endpoints) {
+  if (router.currentRoute.value.path.startsWith(endpoint.url)) {
+    selected.value = endpoint.name;
+    break;
+  }
+}
 
 function toggleLeftDrawer() {
-    leftDrawerOpen.value = !leftDrawerOpen.value
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+function hasPermission(permission) {
+  return checkIfAccountHasPermission(permission);
+}
+
+function changeSelected(name) {
+  selected.value = name;
 }
 </script>
--- website/src/pages/IndexPage.vue
+++ website/src/pages/IndexPage.vue
@@ -1,33 +1,33 @@
 <template>
-    <q-page class="row items-center justify-evenly">
-        <q-color v-model="color" @change="submit"/>
-    </q-page>
+  <q-page class="row items-center justify-evenly">
+    <q-color v-model="color" @change="submit"/>
+  </q-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import {ref} from 'vue';
 import {ws, WebSocketEventCodes} from 'boot/websocket';
 
 let color = ref('#000000');
 
 function hexToRgb(hex: string) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
 }
 
 function submit() {
-    const c = hexToRgb(color.value);
-    if (!c) {
-        console.error('Invalid color');
-        return;
-    }
-    ws.send(JSON.stringify({
-        eventCode: WebSocketEventCodes.DEBUG_SET_LED_BRIGHTNESS.valueOf(),
-        data: {r: c.r, g: c.g, b: c.b}
-    }))
+  const c = hexToRgb(color.value);
+  if (!c) {
+    console.error('Invalid color');
+    return;
+  }
+  ws.send(JSON.stringify({
+    eventCode: WebSocketEventCodes.DEBUG_SET_LED_BRIGHTNESS.valueOf(),
+    data: {r: c.r, g: c.g, b: c.b}
+  }))
 }
 </script>
